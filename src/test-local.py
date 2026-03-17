e025a9b9743815024b3d28bf7288671b26096ca7
Huynh Ngoc Son <huynhngocson.uit.2000@gmail.com>
2026-03-18 01:12:25 +0700

    here

--- src/test-local.py
+++ src/test-local.py
@@ -5,12 +5,15 @@ start_time = time.time()
             
 extractor = DocumentExtractor(cpu=True)
 
-result = extractor.extract("sample-1 1.docx") 
+# result = extractor.extract("test-docx1/sample-1.docx") 
+result = extractor.extract("sample.docx") 
 
-text = result.extract_data(specified_fields=[
-    "Heading 2",
-])
- 
+# case 1
+# text = result.extract_data()
+
+# case 2
+text = result.extract_data()
+
 
 print(text)
 elapsed_time = time.time() - start_time
