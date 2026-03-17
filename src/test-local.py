2e3c50d6a4d8e18691fca4abad730e743e8187d7
Huynh Ngoc Son <huynhngocson.uit.2000@gmail.com>
2026-03-18 00:43:32 +0700

    out put here

--- src/test-local.py
+++ src/test-local.py
@@ -5,10 +5,20 @@ start_time = time.time()
             
 extractor = DocumentExtractor(cpu=True)
 
-result = extractor.extract("sample-123.pdf")
+result = extractor.extract("sample-1 1.docx")
+# result = extractor.extract("sample-5.pdf")
 
 # EXTRACT TEXT LOCAL MODE.... 1 PAGE
-text = result.extract_data() 
+text = result.extract_data(specified_fields=[
+    "Heading 2",
+])
 
+
+# specified_fields=[
+#     'Study Design',
+#     'Schedule of activities'
+# ]
+
+print(text)
 elapsed_time = time.time() - start_time
 print(f"✅ SUCCESS: Elapsed: {elapsed_time:.3f}s") 
